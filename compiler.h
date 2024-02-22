8612bb43b67ce7e2a374622baa5b86e2f1ac8716
Joshua Ewulo <jewulo@gmail.com>
2024-02-22 01:00:27 +0000

    Completed Chapter 16. No Errors so far. The part that was crashing on my first attempt through the book is working fine now. Praise God.

---
+++ compiler.h
@@ -0,0 +1,10 @@
+//
+// Created by jewul on 21/02/2024.
+//
+
+#ifndef CLOX_COMPILER_H
+#define CLOX_COMPILER_H
+
+void compile(const char* source);
+
+#endif //CLOX_COMPILER_H
--- vm.c
+++ vm.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 
 #include "common.h"
+#include "compiler.h"
 #include "debug.h"
 #include "vm.h"
 
@@ -78,9 +79,13 @@ static InterpretResult run() {
 #undef BINARY_OP
 }
 
-InterpretResult interpret(Chunk* chunk) {
+InterpretResult interpret_chunk(Chunk* chunk) {
     vm.chunk = chunk;
     vm.ip = vm.chunk->code;
     return run();
 }
 
+InterpretResult interpret(const char* source) {
+    compile(source);
+    return INTERPRET_OK;
+}
