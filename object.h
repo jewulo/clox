d875eb3ec4b3c0186730d3af0c103fd7e02c80d8
Joshua Ewulo <jewulo@gmail.com>
2024-02-24 22:57:18 +0000

    Stopped at 20.4.2. It is 23:00 and I am sleepy.

--- object.h
+++ object.h
@@ -25,9 +25,10 @@ struct Obj {
 };
 
 struct ObjString {
-    Obj     obj;
-    int     length;
-    char*   chars;
+    Obj         obj;
+    int         length;
+    char*       chars;
+    uint32_t    hash;
 };
 
 ObjString *takeString(char *chars, int length);
