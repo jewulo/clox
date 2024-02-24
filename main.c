8354e267c3cfd2669bda2bb654d8deba387c7511
Joshua Ewulo <jewulo@gmail.com>
2024-02-24 12:17:45 +0000

    Completed Chapter 18. But it is buggy. ! is not working.

--- main.c
+++ main.c
@@ -68,29 +68,28 @@ void test_vm_stack_and_opcode() {
     Chunk chunk;
     initChunk(&chunk);
 
-    int constant = addConstant(&chunk, 1.2);
-    writeChunk(&chunk, OP_CONSTANT, 123);
-    writeChunk(&chunk, constant,123);
+//    int constant = addConstant(&chunk, 1.2);
+//    writeChunk(&chunk, OP_CONSTANT, 123);
+//    writeChunk(&chunk, constant,123);
+//
+//    constant = addConstant(&chunk, 3.4);
+//    writeChunk(&chunk, OP_CONSTANT, 123);
+//    writeChunk(&chunk, constant,123);
+//
+//    writeChunk(&chunk, OP_ADD, 123);
+//
+//    constant = addConstant(&chunk, 5.6);
+//    writeChunk(&chunk, OP_CONSTANT, 123);
+//    writeChunk(&chunk, constant,123);
+//
+//    writeChunk(&chunk, OP_DIVIDE, 123);
+//    writeChunk(&chunk, OP_NEGATE, 123);
+//
+//    writeChunk(&chunk, OP_RETURN, 123);
+//    disassembleChunk(&chunk, "test chunk");
 
-    constant = addConstant(&chunk, 3.4);
-    writeChunk(&chunk, OP_CONSTANT, 123);
-    writeChunk(&chunk, constant,123);
-
-    writeChunk(&chunk, OP_ADD, 123);
-
-    constant = addConstant(&chunk, 5.6);
-    writeChunk(&chunk, OP_CONSTANT, 123);
-    writeChunk(&chunk, constant,123);
-
-    writeChunk(&chunk, OP_DIVIDE, 123);
-    writeChunk(&chunk, OP_NEGATE, 123);
-
-    writeChunk(&chunk, OP_RETURN, 123);
-    disassembleChunk(&chunk, "test chunk");
     freeVM();
     interpret_chunk(&chunk);
-    freeVM();
-    freeChunk(&chunk);
 }
 
 void main_helper(int argc, const char *argv[]) {
--- value.h
+++ value.h
@@ -5,14 +5,43 @@
 #ifndef CLOX_VALUE_H
 #define CLOX_VALUE_H
 
-typedef double Value;
+#include "common.h"
 
+typedef enum {
+    VAL_BOOL,
+    VAL_NIL,
+    VAL_NUMBER
+} ValueType;
+
+// typedef double Value;
+typedef struct {
+    ValueType type;
+    union {
+        bool    boolean;
+        double  number;
+    } as;
+} Value;
+
+// clox type verifiers
+#define IS_BOOL(value)      ((value).type == VAL_BOOL)
+#define IS_NIL(value)       ((value).type == VAL_NIL)
+#define IS_NUMBER(value)    ((value).type == VAL_NUMBER)
+
+// convert clox's dynamic types to C-language static types
+#define AS_BOOL(value)             ((value).as.boolean)
+#define AS_NUMBER(value)           ((value).as.number)
+
+// convert C-language static types to clox's dynamic types
+#define BOOL_VAL(value)     ((Value){VAL_BOOL,      {.boolean = value}})
+#define NIL_VAL             ((Value){VAL_NIL,       {.number = 0}})
+#define NUMBER_VAL(value)   ((Value){VAL_NUMBER,    {.number = value}})
 typedef struct {
     int capacity;
     int count;
     Value* values;
 } ValueArray;
 
+bool valuesEqual(Value a, Value b);
 void initValueArray(ValueArray* array);
 void writeValueArray(ValueArray* array, Value value);
 void freeValueArray(ValueArray* array);
