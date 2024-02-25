5540447d554dd89cca6cd85efffc53bbbabe997b
Joshua Ewulo <jewulo@gmail.com>
2024-02-25 14:32:20 +0000

    Stopped at 20.4.5. Need a break. Deleting from the table looks complicated.

--- table.h
+++ table.h
@@ -19,7 +19,13 @@ typedef struct  {
     Entry*  entries;
 } Table;
 
+#define TABLE_MAX_LOAD 0.75
+
 void initTable(Table* table);
 void freeTable(Table* table);
+bool tableGet(Table* table, ObjString* key, Value* value);
+bool tableSet(Table* table, ObjString* key, Value value);
+bool tableDelete(Table* table, ObjString* key);
+void tableAddAll(Table* from, Table* to);
 
 #endif //CLOX_TABLE_H
