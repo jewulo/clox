2b235b8e28a64a36b55fd19b3616b795a3534338
Joshua Ewulo <jewulo@gmail.com>
2024-03-04 18:02:49 +0000

    Started Chapter 25. clox now has closures. All ObjFunction types are now wrapped in a ObjClosure type. Amazing book.

--- vm.c
+++ vm.c
@@ -6,6 +6,7 @@
 #include <string.h>
 #include <time.h>
 
+#include "chunk.h"
 #include "common.h"
 #include "compiler.h"
 #include "debug.h"
@@ -52,7 +53,7 @@ static void runtimeError(const char* format, ...) {
 
     for (int i = vm.frameCount - 1; i >= 0; i--) {
         CallFrame* frame = &vm.frames[i];
-        ObjFunction* function = frame->function;
+        ObjFunction* function = frame->closure->function;
         size_t instruction = frame->ip - function->chunk.code - 1;
 
         fprintf(stderr, "[line %d] in ",
@@ -106,6 +107,9 @@ static Value peek(int distance) {
     return vm.stackTop[-1 - distance];
 }
 
+/*          REMOVED CHAPTER 25. PAGE 486.
+ *          replaced with:
+ *          static bool call(ObjClosure* function, int argCount)
 // execute a lox function call
 static bool call(ObjFunction* function, int argCount) {
     if (argCount != function->arity) {
@@ -125,12 +129,38 @@ static bool call(ObjFunction* function, int argCount) {
     frame->slots = vm.stackTop - argCount - 1;
     return true;
 }
+*/
+
+// execute a lox closure (function & environment)
+static bool call(ObjClosure* closure, int argCount) {
+    if (argCount != closure->function->arity) {
+        runtimeError("Expected %d arguments but got %d.",
+                     closure->function->arity, argCount);
+        return false;
+    }
+
+    if (vm.frameCount == FRAMES_MAX) {
+        runtimeError("Stack overflow.");
+        return false;
+    }
+
+    CallFrame* frame = &vm.frames[vm.frameCount++];
+    frame->closure = closure;
+    frame->ip = closure->function->chunk.code;
+    frame->slots = vm.stackTop - argCount - 1;
+    return true;
+}
 
 static bool callValue(Value callee, int argCount) {
     if (IS_OBJ(callee)) {
         switch (OBJ_TYPE(callee)) {
+            case OBJ_CLOSURE:
+                return call(AS_CLOSURE(callee), argCount);
+
+/*          REMOVED CHAPTER 25. PAGE 486.
             case OBJ_FUNCTION:
                 return call(AS_FUNCTION(callee), argCount);
+*/
             case OBJ_NATIVE: {
                 NativeFn native = AS_NATIVE(callee);
                 Value result = native(argCount, vm.stackTop - argCount);
@@ -173,9 +203,10 @@ static InterpretResult run() {
 #define READ_SHORT() (frame->ip += 2, \
                      (uint16_t)((frame->ip[-2] << 8) | frame->ip[-1]))
 
-#define READ_CONSTANT() (frame->function->chunk.constants.values[READ_BYTE()])
+#define READ_CONSTANT() (frame->closure->function->chunk.constants.values[READ_BYTE()])
 
 #define READ_STRING() AS_STRING(READ_CONSTANT())
+
 #define BINARY_OP(valueType, op) \
     do {                         \
         if (!IS_NUMBER(peek(0)) || !IS_NUMBER(peek(1))) { \
@@ -196,8 +227,8 @@ static InterpretResult run() {
             printf(" ]");
         }
         printf("\n");
-        disassembleInstruction(&frame->function->chunk,
-                               (int)(frame->ip - frame->function->chunk.code));
+        disassembleInstruction(&frame->closure->function->chunk,
+                               (int)(frame->ip - frame->closure->function->chunk.code));
 #endif
         uint8_t instruction;
         switch (instruction = READ_BYTE()) {
@@ -309,6 +340,12 @@ static InterpretResult run() {
                 frame = &vm.frames[vm.frameCount - 1];
                 break;
             }
+            case OP_CLOSURE: {
+                ObjFunction* function = AS_FUNCTION(READ_CONSTANT());
+                ObjClosure* closure = newClosure(function);
+                push(OBJ_VAL(closure));
+                break;
+            }
             case OP_RETURN: {
                 Value result = pop();   // get value expression being returned
                 vm.frameCount--;        // redcece frames
@@ -365,7 +402,10 @@ InterpretResult interpret(const char* source) {
     if (function == NULL) return INTERPRET_COMPILE_ERROR;
 
     push(OBJ_VAL(function));
-    call(function, 0);
+    ObjClosure* closure = newClosure(function);
+    pop();
+    push(OBJ_VAL(closure));
+    call(closure, 0);
 
     return run();
 }
