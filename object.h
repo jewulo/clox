b6a036a45d4eed9256e94adc76c5c6c1fe839384
Joshua Ewulo <jewulo@gmail.com>
2024-03-04 14:43:40 +0000

    Started Chapter 25. Stopped for break at 25.1.2.

--- object.h
+++ object.h
@@ -12,16 +12,19 @@
 
 #define OBJ_TYPE(value)         (AS_OBJ(value)->type)
 
+#define IS_CLOSURE(value)       isObjType(value, OBJ_CLOSURE)
 #define IS_FUNCTION(value)      isObjType(value, OBJ_FUNCTION)
 #define IS_NATIVE(value)        isObjType(value, OBJ_NATIVE)
 #define IS_STRING(value)        isObjType(value, OBJ_STRING)
 
+#define AS_CLOSURE(value)       ((ObjClosure *)AS_OBJ(value))
 #define AS_FUNCTION(value)      ((ObjFunction*)AS_OBJ(value))
 #define AS_NATIVE(value)        (((ObjNative*)AS_OBJ(value))->function)
 #define AS_STRING(value)        ((ObjString*)AS_OBJ(value))
 #define AS_CSTRING(value)       (((ObjString*)AS_OBJ(value))->chars)
 
 typedef enum {
+    OBJ_CLOSURE,
     OBJ_FUNCTION,
     OBJ_NATIVE,
     OBJ_STRING,
@@ -33,17 +36,17 @@ struct Obj {
 };
 
 typedef struct {
-    Obj obj;
-    int arity;
-    Chunk chunk;
-    ObjString* name;
+    Obj         obj;
+    int         arity;
+    Chunk       chunk;
+    ObjString*  name;
 } ObjFunction;
 
 typedef Value (*NativeFn)(int argCount, Value* args);
 
 typedef struct {
-    Obj obj;
-    NativeFn function;
+    Obj         obj;
+    NativeFn    function;
 } ObjNative;
 
 // representing clox strings has list of chars
@@ -54,6 +57,12 @@ struct ObjString {
     uint32_t    hash;
 };
 
+typedef struct {
+    Obj             obj;
+    ObjFunction*    function;
+} ObjClosure;
+
+ObjClosure* newClosure(ObjFunction* function);
 ObjFunction* newFunction();
 ObjNative* newNative(NativeFn function);
 ObjString* takeString(char *chars, int length);
