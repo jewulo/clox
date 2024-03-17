2014432664bc6d3b9dc7fd50e8bb26da16f868f5
Joshua Ewulo <jewulo@gmail.com>
2024-03-17 21:02:02 +0000

    Completed Chapter 27. CLox has gotten rudimentary data only objects.

--- debug.c
+++ debug.c
@@ -76,6 +76,10 @@ int disassembleInstruction(Chunk *chunk, int offset) {
             return byteInstruction("OP_GET_UPVALUE", chunk, offset);
         case OP_SET_UPVALUE:
             return byteInstruction("OP_SET_UPVALUE", chunk, offset);
+        case OP_GET_PROPERTY:
+            return constantInstruction("OP_GET_PROPERTY", chunk, offset);
+        case OP_SET_PROPERTY:
+            return constantInstruction("OP_SET_PROPERTY", chunk, offset);
         case OP_EQUAL:
             return simpleInstruction("OP_EQUAL", offset);
         case OP_GREATER:
@@ -119,7 +123,6 @@ int disassembleInstruction(Chunk *chunk, int offset) {
                        offset - 2, isLocal ? "local" : "upvalue", index);
 
             }
-
             return offset;
         case OP_CLOSE_UPVALUE:
             return simpleInstruction("OP_CLOSE_UPVALUE", offset);
